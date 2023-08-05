c0415c3635c14d8baed690f1b0f0726bf595db7b
Muhammad. Talha Iqbal <88932788+QariTheDev@users.noreply.github.com>
2023-08-05 18:27:51 +0500

    Update sanity.cli.js

--- backend_sanity/sanity.cli.js
+++ backend_sanity/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '552nodxx',
+    projectId: '(Add your Project Id)',
     dataset: 'production'
   }
 })
